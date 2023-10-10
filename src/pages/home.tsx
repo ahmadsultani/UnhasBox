2a4081f1418ca28b4dfc6907eb423d12f95f23ef
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-10-10 23:54:46 +0800

    feat: homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,10 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
+  IonButton,
+  IonGrid,
+  IonCol,
+  IonRow,
 } from "@ionic/react";
 import { Navbar } from "../components/Navbar";
 
@@ -11,12 +15,27 @@ export const Home: React.FC = () => {
   return (
     <IonPage>
       <Navbar title="Home" />
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-text-center">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <h1 className="ion-margin-bottom">Welcome to Our Website</h1>
+            <p className="ion-margin-bottom">Discover amazing products and services.</p>
+            <IonButton routerLink="/signup" className="ion-margin" >
+              Get Started
+            </IonButton>
+            <IonButton routerLink="/signup" >
+              Get Started
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      
       </IonContent>
     </IonPage>
   );
