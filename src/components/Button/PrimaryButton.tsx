cb9a47e84b76027926c64009ab1b1062b5495701
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-10-11 00:10:52 +0800

    fix: no branch

---
+++ src/components/Button/PrimaryButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { IonButton} from "@ionic/react";
+ 
+interface PrimaryButtonProps {
+   children: React.ReactNode;
+   onClick: () => void;
+   fullWidth?: boolean;
+   size?: "small" | "default" | "large";
+}
+ 
+export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ 
+   children, 
+   onClick,
+   fullWidth,
+   size,
+}) => {
+
+   return (
+     <IonButton className="ion-text-capitalize" onClick={onClick} expand={fullWidth ? "full" : undefined} size={size}>
+         {children}
+     </IonButton>
+   );
+};
