425f431e2e2dcf36140432ef966a087839b8de06
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-10-13 01:26:33 +0800

    feat: responsive 404 page

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,37 +1,22 @@
-import { IonButton, IonContent, IonPage } from "@ionic/react";
-import { Link } from "react-router-dom";
+import { IonButton, IonCol, IonText } from "@ionic/react";
 
-import React from "react";
+import MainLayout from "../layouts/MainLayout";
 
 import "../styles/_404.css";
-import { Navbar } from "../components/Navbar";
-import { Footer } from "../components/Footer";
 
 export const _404: React.FC = () => {
   return (
-    <IonPage>
-      <Navbar
-        title="Not Found Page"
-        onBackClick={() => {
-          window.history.back();
-        }}
-        hasBackButton
-      />
-      <IonContent fullscreen>
-        <section className="not-found">
-          <h1 className="not-found__heading">Oops! Page Not Found</h1>
-          <div className="not-found__description">
-            <p>We're sorry, but the page you're looking for cannot be found.</p>
-            <p>
-              You can go back to the homepage or contact support for assistance.
-            </p>
-          </div>
-          <Link to="/">
-            <IonButton>Go Home</IonButton>
-          </Link>
-        </section>
-      </IonContent>
-      <Footer />
-    </IonPage>
+    <MainLayout>
+      <IonCol className="ion-padding not-found">
+        <IonText className="not-found__heading">Oops! Page Not Found</IonText>
+        <IonText className="not-found__description" color="dark">
+          <p>We're sorry, but the page you're looking for cannot be found.</p>
+          <p>
+            You can go back to the homepage or contact support for assistance.
+          </p>
+        </IonText>
+        <IonButton href="/">Go Home</IonButton>
+      </IonCol>
+    </MainLayout>
   );
 };
