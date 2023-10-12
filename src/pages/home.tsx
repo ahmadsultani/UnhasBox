012c23813cd3cbfed8187cc1c29e36a25fb4769d
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-10-13 01:26:45 +0800

    feat: implement main layout on home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,30 +1,30 @@
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
-import { Navbar } from "../components/Navbar";
+import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+
 import { PrimaryButton, OutlinedButton } from "../components/Button";
 
+import MainLayout from "../layouts/MainLayout";
 
 export const Home: React.FC = () => {
   const handleButtonClick = () => {
     console.log("Tombol Kembali diklik");
-  }
+  };
   return (
-    <IonPage>
-      <Navbar title="Home" />
-      <IonContent fullscreen>
-        <PrimaryButton onClick={handleButtonClick} fullWidth={false} size="small">Button</PrimaryButton>
-        <OutlinedButton onClick={handleButtonClick} fullWidth={false} size="small">Button</OutlinedButton>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-      </IonContent>
-    </IonPage>
+    <MainLayout>
+      <PrimaryButton onClick={handleButtonClick} fullWidth={false} size="small">
+        Button
+      </PrimaryButton>
+      <OutlinedButton
+        onClick={handleButtonClick}
+        fullWidth={false}
+        size="small"
+      >
+        Button
+      </OutlinedButton>
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </MainLayout>
   );
 };
