c93d680e3ab67a3504b5c5b3aedb05b6b464233a
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-10-22 08:37:51 +0800

    feat: blog dummy data

---
+++ src/data/fakeBlogData.ts
@@ -0,0 +1,84 @@
+interface IBlog {
+  id: number,
+  slug: string;
+  title: string;
+  tags: string[];
+  content: string;
+  image: string;
+}
+
+export const blogs: IBlog[] = [
+  {
+    id: 1,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content:
+      "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 2,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 3,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 4,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 5,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 6,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 7,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+  {
+    id: 8,
+    slug: "blog-123",
+    title: "Blog Title 1",
+    tags: ["ini tag1", "ini tag2", "ini tag3", "ini tag3"],
+    content: "Blog Content 1",
+    image:
+      "https://www.adorama.com/alc/wp-content/uploads/2021/06/mountain-photography-tips-tricks-feature.jpg",
+  },
+];
