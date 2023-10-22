fc932c419a7985c16544d3df9ef8fefe0ceb689b
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-10-22 08:36:50 +0800

    fix: never target html tag style

--- src/components/Cart/EmptyStateCart.tsx
+++ src/components/Cart/EmptyStateCart.tsx
@@ -7,7 +7,7 @@ export const EmptyStateCart: React.FC = () => {
       <IonCol className="cart-container ion-padding ion-text-center">
         <IonIcon className="cart__icon" icon={cartOutline}></IonIcon>
         <h4><b>Your Cart is Empty</b></h4>
-        <p className="ion-margin-bottom">You haven't added anything to your cart. We're sure you'll find something in our store</p>
+        <p className="cart__text ion-margin-bottom">You haven't added anything to your cart. We're sure you'll find something in our store</p>
         <IonButton shape="round" className="ion-margin-top">Shop Now</IonButton>
       </IonCol>
   );
--- src/components/Favorite/EmptyStateFav.tsx
+++ src/components/Favorite/EmptyStateFav.tsx
@@ -5,7 +5,7 @@ export const EmptyStateFav: React.FC = () => {
   return (
       <IonCol className="myFav-container ion-padding ion-text-center">
         <h4><b>My Favorite is Still Empty</b></h4>
-        <p>Let's start adding your dream products here!</p>
+        <p className="myFav__text">Let's start adding your dream products here!</p>
       </IonCol>
   );
 };
