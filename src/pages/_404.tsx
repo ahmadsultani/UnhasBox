8778f12bf344ba4a694fe79861e63b01c7b34e8d
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2023-10-22 08:30:56 +0800

    feat: back button on 404

--- src/pages/_404.tsx
+++ src/pages/_404.tsx
@@ -1,10 +1,13 @@
 import { IonButton, IonCol, IonText } from "@ionic/react";
+import { useHistory } from "react-router";
 
 import { MainLayout } from "../layouts";
 
 import "../styles/_404.css";
 
 export const _404: React.FC = () => {
+  const history = useHistory();
+
   return (
     <MainLayout>
       <IonCol className="not-found">
@@ -15,7 +18,24 @@ export const _404: React.FC = () => {
             You can go back to the homepage or contact support for assistance.
           </p>
         </IonText>
-        <IonButton href="/">Go Home</IonButton>
+
+        <section className="not-found__link-container">
+          <IonButton
+            shape="round"
+            size="small"
+            fill="clear"
+            onClick={() => history.goBack()}
+          >
+            <IonText>
+              Go Back
+            </IonText>
+          </IonButton>
+          <IonButton size="small" shape="round" href="/">
+            <IonText className="ion-padding">
+              Go Home
+            </IonText>
+          </IonButton>
+        </section>
       </IonCol>
     </MainLayout>
   );
