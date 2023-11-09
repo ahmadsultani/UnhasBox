176ce5b88580b6e9199991d5fedb7bdc2c8c4702
misnaaaaws <misnawati240804@gmail.com>
2023-11-09 14:51:44 +0800

    feat: add fake cart data

---
+++ src/data/fakeCartData.ts
@@ -0,0 +1,13 @@
+export const fakeCartData = {
+    total_price : 1000000,
+    total_product : 1,
+    products : [
+        {
+            id: 1,
+            slug: "as",
+            title: "Product 1",
+            price: 500000,
+            image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg"
+        },
+    ],
+};
