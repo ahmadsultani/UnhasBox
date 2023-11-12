3f394cd100f2ed326de3681a6452fe857d861722
ahmadsultani <dayanullahas21d@student.unhas.ac.id>
2023-11-12 21:26:25 +0800

    feat: init react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import App from "./App";
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
