ab2001fb9a6f8c5348866aa7607d91a72eb7f901
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-13 10:25:23 +0800

    feat: change price data type

--- src/data/fakeProductData.ts
+++ src/data/fakeProductData.ts
@@ -3,7 +3,7 @@ interface dataProduct {
   slug: string;
   title: string;
   category: string[];
-  price: string;
+  price: number;
   image: string;
   desc: string;
 }
@@ -13,7 +13,7 @@ export const products: dataProduct[] = [
     id: 1,
     slug: "product-123",
     title: "Product 1",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -22,7 +22,7 @@ export const products: dataProduct[] = [
     id: 2,
     slug: "product-123",
     title: "Product 2",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -31,7 +31,7 @@ export const products: dataProduct[] = [
     id: 3,
     slug: "product-123",
     title: "Product 3",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -40,7 +40,7 @@ export const products: dataProduct[] = [
     id: 4,
     slug: "product-123",
     title: "Product 4",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -49,7 +49,7 @@ export const products: dataProduct[] = [
     id: 5,
     slug: "product-123",
     title: "Product 5",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -58,7 +58,7 @@ export const products: dataProduct[] = [
     id: 6,
     slug: "product-123",
     title: "Product 6",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -67,7 +67,7 @@ export const products: dataProduct[] = [
     id: 7,
     slug: "product-123",
     title: "Product 7",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
@@ -76,7 +76,7 @@ export const products: dataProduct[] = [
     id: 8,
     slug: "product-123",
     title: "Product 8",
-    price: "500.000",
+    price: 50000000,
     category: ["Category-1", "Category-2"],
     image: "https://cdn.eraspace.com/media/catalog/product/a/p/apple_watch_ultra_49mm_gps_cellular_titanium_case_with_yellow_ocean_band_1_1_1.jpg",
     desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis reprehenderit neque alias voluptatibus exercitationem dolore."
