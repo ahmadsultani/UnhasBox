fb2fdd9214997315797beb2309c3909320d77987
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-13 21:40:27 +0800

    feat: change title

--- src/components/ProductSection/ProductSection.tsx
+++ src/components/ProductSection/ProductSection.tsx
@@ -1,3 +1,5 @@
+import { IonButton } from "@ionic/react";
+
 import { ProductCard } from "@/components/ProductCard";
 
 import { products } from "@/data/fakeProductData";
@@ -6,15 +8,25 @@ import "@/styles/product-section.css";
 
 export const ProductSection: React.FC = () => {
   const firstFourProducts = products.slice(0, 4);
-
   return (
     <main className="product-section">
-      <h1 className="product-section__title">New Products</h1>
-      <main className="product-section__container">
+      <h1 className="product-section__title">Top Picks</h1>
+      <section className="product-section__container">
           {firstFourProducts.map((product) => (
-            <ProductCard key={product.id} slug={product.slug} title={product.title} category={product.category} price={product.price} image={product.image} desc={product.desc} />
+            <ProductCard
+              key={product.id}
+              slug={product.slug}
+              title={product.title}
+              category={product.category}
+              price={product.price}
+              image={product.image}
+              desc={product.desc}
+              />
           ))}
-        </main>
+      </section>
+      <section>
+        <IonButton href="/blog" fill="solid" color='primary' className="product-section__button">Explore Product</IonButton>
+      </section>
     </main>
   );
 };
