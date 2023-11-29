7b258f1c1cb0acba1867e8d63680861800ab0aab
izzahnin <nurhidayatni21d@student.unhas.ac.id>
2023-11-29 17:04:35 +0800

    feat: change hero background

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,23 +1,34 @@
-import { 
-  IonButton, IonImg, IonText,
-} from '@ionic/react';
-import React from 'react';
+import { IonButton, IonImg, IonText } from "@ionic/react";
+import React from "react";
 import "@/styles/hero.css";
 
 export const Hero: React.FC = () => {
   return (
-    <div className='hero'>
-      <IonImg src="https://images.unsplash.com/photo-1563244495-1de93ffe7c23?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="rektorat unhas" className="hero__image" />
-      <section className='hero__text'>
+    <div className="hero">
+      <IonImg
+        src="https://i.ytimg.com/vi/uaVVSvP-5yg/maxresdefault.jpg"
+        alt="rektorat unhas"
+        className="hero__image"
+      />
+      <section className="hero__text">
         <IonText>
-          <h1 className='hero__text-heading'>Welcome to Our Website</h1>
-          <p className='hero__text-p'>Discover Amaizing Products</p>
+          <h1 className="hero__text-heading">Welcome to Our Website</h1>
+          <p className="hero__text-p">Discover Amaizing Products</p>
         </IonText>
-        <div className='hero__text-buttons'>
-          <IonButton href="/product" fill="solid" color='primary' className="hero__text-button">Explore Product</IonButton>
-          <IonButton fill="outline" color='light' className="hero__text-button">Signup</IonButton>
+        <div className="hero__text-buttons">
+          <IonButton
+            href="/product"
+            fill="solid"
+            color="primary"
+            className="hero__text-button"
+          >
+            Explore Product
+          </IonButton>
+          <IonButton fill="outline" color="light" className="hero__text-button">
+            Signup
+          </IonButton>
         </div>
-      </section>  
+      </section>
     </div>
   );
 };
